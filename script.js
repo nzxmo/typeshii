ea68f49b5b85ca4750e44b488676c6d5e87e7699
JAMES <yrjamesnz@gmail.com>
2025-02-04 07:16:48 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+// Add interactivity if needed
+const flower = document.querySelector('.flower');
+
+flower.addEventListener('click', () => {
+  alert("You make my life beautiful! 🌸");
+});
